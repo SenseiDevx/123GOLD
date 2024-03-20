c686f42b2436281ccd47e412a513942ed4f1610d
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2024-03-20 17:58:01 +0600

    add some details

--- src/Components/MainPage/Categories/Categories.jsx
+++ src/Components/MainPage/Categories/Categories.jsx
@@ -1,15 +1,23 @@
 import cls from "./Categories.module.css"
-import { topRingData } from "../../../consts/ringData"; // Импортируйте данные
+import { topRingData } from "../../../consts/ringData";
+import {useNavigate} from "react-router-dom"; // Импортируйте данные
 
 
 const Categories = () => {
+
+    const navigate = useNavigate()
+
+    const toRingPage = () => {
+        navigate("/ring-page/")
+    }
+
     return (
         <div className="container">
             <div className={cls.wrapper}>
                 <h1 className={cls.h1}>categories</h1>
                 <div className={cls.topRings}>
                     {topRingData.map((ring) => (
-                        <div key={ring.id} className={cls.ringBlock}>
+                        <div key={ring.id} className={cls.ringBlock} onClick={toRingPage}>
                             <img src={ring.image} alt={`Ring ${ring.id}`} />
                             <h4 className={cls.h4}>{ring.status}</h4>
                             <p className={cls.p}>{ring.description}</p>
--- src/pages/AboutRing/AboutRing.jsx
+++ src/pages/AboutRing/AboutRing.jsx
@@ -3,8 +3,14 @@ import LeftRingImage from "../../Components/AboutRing/LeftRingImage/LeftRingImag
 import RightCustomize from "../../Components/AboutRing/RightCustomize/RightCustomize";
 import ArticleDetails from "../../Components/AboutRing/ArticleDetails/ArticleDetails";
 import ShareToSocial from "../../Components/AboutRing/ShareToSocial/ShareToSocial";
+import {useEffect} from "react";
 
 const AboutRing = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div className="container">
             <div className={cls.item1}>
