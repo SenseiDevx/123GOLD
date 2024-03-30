6235b0953344cb2dca4691583cde77a01e17f285
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2024-03-30 17:20:08 +0600

    done contacts page

--- src/Components/Contacts/ContactInput/ContactInput.jsx
+++ src/Components/Contacts/ContactInput/ContactInput.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import cls from "./ContactInput.module.css"
 
 const ContactInput = () => {
     return (
-        <div>
-            
+        <div className={cls.wrapper}>
+            <h3 className={cls.h3}>WHERE AM I?</h3>
+            <div className={cls.parentInput}>
+                <input className={cls.input} type="text" placeholder="location"/>
+                <button className={cls.search}>Search</button>
+            </div>
         </div>
     );
 };
