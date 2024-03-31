65debc3987a0e2e8586f178488ab3bbef93ec63c
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2024-03-31 14:23:09 +0600

    done jewelery page

--- src/pages/Jewelery/Jewelery.jsx
+++ src/pages/Jewelery/Jewelery.jsx
@@ -4,6 +4,7 @@ import JeweleryRings from "../../Components/Jewelery/JeweleryRings/JeweleryRings
 import AllGoldGoes from "../../Components/Jewelery/AllGoldGoes/AllGoldGoes";
 import ForYou from "../../Components/MainPage/ForYou/ForYou";
 import DiversityBg from "../../Components/Jewelery/DiversityBg/DiversityBg";
+import JeweleryService from "../../Components/Jewelery/JeweleryService/JeweleryService";
 
 const Jewelery = () => {
     return (
@@ -15,6 +16,9 @@ const Jewelery = () => {
                 <ForYou/>
             </div>
             <DiversityBg/>
+            <div className="container">
+                <JeweleryService/>
+            </div>
         </div>
     );
 };
